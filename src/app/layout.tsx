48443ca12c28f190acc5aafafbc0a3f5bd3294a0
raphael-caninde <raphael.caninde@gmail.com>
2023-06-07 15:39:46 -0300

    config prettier e eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: '',
-  description: '',
-}
+  description: ''
+};
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='pt-br'>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
