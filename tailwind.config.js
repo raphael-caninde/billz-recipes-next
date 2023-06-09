5e8d3baa516c5e9953b29c07a3617cf87ec0c2f1
raphael-caninde <raphael.caninde@gmail.com>
2023-06-08 23:58:17 -0300

    add cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,15 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      
-    },
+      colors: {
+        'recipe-app': '#001845',
+        'recipe-app-yellow': '#ffd60a'
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
