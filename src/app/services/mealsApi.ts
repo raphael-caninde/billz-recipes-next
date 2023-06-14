78fb51d58a6523c21e9ba8e6a63e90dc4a1f100c
raphael-caninde <raphael.caninde@gmail.com>
2023-06-14 11:48:48 -0300

    feat: fetch api

---
+++ src/app/services/mealsApi.ts
@@ -0,0 +1,19 @@
+import { axiosiInstance } from './api';
+
+export async function getFoodsSearch(food = '') {
+  const foods = await axiosiInstance.get(`/search.php?s=${food}`);
+
+  return foods.data;
+}
+
+export async function getCategory() {
+  const category = await axiosiInstance.get('/categories.php');
+
+  return category.data;
+}
+
+export async function getCategoryFilter(category: string) {
+  const filterCategory = await axiosiInstance.get(`/filter.php?c=${category}`);
+
+  return filterCategory.data;
+}
