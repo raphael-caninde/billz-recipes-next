c8922601b33aa5be25b6d98f1aad9c5734136842
raphael-caninde <raphael.caninde@gmail.com>
2023-06-15 14:28:02 -0300

    feat: config react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,25 @@
-import './globals.css';
+'use client';
+
 import { Inter } from 'next/font/google';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { Header } from './components/Header';
+import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: '',
-  description: ''
+  title: 'Bilz - Recipes',
+  description: 'Aplicativo de receitas'
 };
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 export default function RootLayout({
   children
@@ -14,8 +27,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='pt-br'>
-      <body className={inter.className}>{children}</body>
+    <html lang='pt-br' className='bg-recipe-app'>
+      <body className={`${inter.className} h-screen w-screen`}>
+        <QueryClientProvider client={queryClient}>
+          <Header />
+          {children}
+        </QueryClientProvider>
+      </body>
     </html>
   );
 }
