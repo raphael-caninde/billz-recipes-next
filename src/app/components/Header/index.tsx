af4ee1be98ba332c9dc1eb3b6838381b4711c0bd
raphael-caninde <raphael.caninde@gmail.com>
2023-06-15 14:38:11 -0300

    feat: header- implementa categorias e letras

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,15 +1,26 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client';
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { useQuery } from 'react-query';
 
+import { latters } from '@/app/const/latters';
 import { faSearch, faUserAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import logo from '../../../../public/logo.png';
+import { getCategory } from '../../services/mealsApi';
+
+interface ICategory {
+  idCategory: string;
+  strCategory: string;
+}
 
 export function Header() {
+  const { data } = useQuery('category', getCategory);
+
+  console.log(data);
+
   return (
     <header className='w-full border-b border-zinc-300'>
       <div
@@ -66,6 +77,46 @@ export function Header() {
           <FontAwesomeIcon className='text-3xl' icon={faUserAlt} />
           <p className={`hidden md:block`}>Login</p>
         </div>
+      </div>
+      <div
+        className='
+          flex w-full gap-4 overflow-x-auto
+          py-4 md:flex-wrap md:justify-center
+        '
+      >
+        {data?.categories.map((category: ICategory) => (
+          <div className='p-2' key={category.idCategory}>
+            <Link
+              className='
+                relative h-full font-medium text-white 
+                before:absolute before:-bottom-1 before:h-0.5 before:w-full
+                before:scale-x-0 before:bg-recipe-app-yellow before:transition
+                hover:before:scale-x-100'
+              href={`/category/${category.strCategory}`}
+            >
+              {category.strCategory}
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div
+        className='
+          flex w-full flex-wrap items-center justify-center divide-x
+        '
+      >
+        {latters.map((latter, i) => (
+          <div className='hidden md:block' key={i + latter}>
+            <Link
+              href={`/teste/${latter}`}
+              className='
+                px-3 py-2 uppercase text-recipe-app-yellow
+                transition-colors hover:text-yellow-600
+              '
+            >
+              {latter}
+            </Link>
+          </div>
+        ))}
       </div>
     </header>
   );
